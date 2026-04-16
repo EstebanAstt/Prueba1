9615fbce0152dd97879a0b3584e97a31de6ae543
EstebanAstt <esteban.astetecif@gmail.com>
2026-04-16 18:39:03 -0400

    Rename class Main40 to example

--- example.java
+++ example.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Main40 {
+public class example {
     public static void main(String[] args) {
         Deposito d1=new Deposito( );
         Deposito d2=new Deposito( );
